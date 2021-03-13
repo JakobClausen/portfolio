4c18dee8b8186e8e815f5b555805321624a33702
Jakob Clausen <jakob.fridmar@gmail.com>
2021-03-13 14:08:28 +0100

    added new project img

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,20 +3,20 @@ export const data = [
     id: 1,
     title: "Portfolio",
     description:
-      "This website is built with NJext js in typescript. \n My goals was to try some different types of animations, like the Scroll magic logo animation. Some of the libs I use is Framer motion, Scroll magic, GSAP 3. \n For most of the styling I opted for Chakra UI components.",
+      "This website is built with Next js in typescript. \n My goals was to try different types of animations, like the Scroll magic logo animation. Some of the libs I use is Framer motion, Scroll magic, GSAP 3. \n For styling i used Chakra ui components.",
   },
   {
     id: 2,
     title: "Gym hub",
     description:
       "This website is built with NJext js in typescript. \n My goals was to try some different types of animations, like the Scroll magic logo animation. Some of the libs I use is Framer motion, Scroll magic, GSAP 3. \n For most of the styling I opted for Chakra UI components.",
-    img: "/images/two.jpg",
+    img: "/images/timer.png",
   },
   {
     id: 3,
     title: "test",
     description:
       "This website is built with NJext js in typescript. \n My goals was to try some different types of animations, like the Scroll magic logo animation. Some of the libs I use is Framer motion, Scroll magic, GSAP 3. \n For most of the styling I opted for Chakra UI components.",
-    img: "/images/three.jpg",
+    img: "/images/backofficeComp.png",
   },
 ];
