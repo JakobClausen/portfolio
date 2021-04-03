0927809d18aa814b8c51d4d23a597e9b5e992c98
Jakob Clausen <jakob.fridmar@gmail.com>
2021-04-03 14:54:14 +0200

    add category name

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,13 +4,31 @@ import { Box, Flex, Image, Text } from "@chakra-ui/react";
 export const About: React.FC = () => {
   return (
     <Box w="100%" h="100vh" p="25px" id="about-me">
+      <Flex alignItems="center">
+        <svg height="4px" width="15px">
+          <line
+            x1="0"
+            x2="100%"
+            stroke="#cecece"
+            style={{ strokeWidth: "4px" }}
+          />
+        </svg>
+        <Text
+          ml="7px"
+          fontFamily="body"
+          color="#cecece"
+          fontWeight="bold"
+          fontSize="18px"
+        >
+          About
+        </Text>
+      </Flex>
       <Flex>
         <Image
           src="/images/jakob.jpeg"
           alt="Jakob Clausen"
           borderRadius="3px"
           w="200px"
-          ml="-35px"
         />
         <Box w="100%" pl="20px">
           {["React", "Next", "Gatsby", "Node", "GraphQL", "SQL"].map((tech) => (
