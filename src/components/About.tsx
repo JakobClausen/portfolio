ce9e1347a4d33b141224e49beb3adb278cbc2fc1
Jakob Clausen <jakob.fridmar@gmail.com>
2021-04-05 18:05:18 +0200

    add breakpoints to text wrap around

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -16,51 +16,82 @@ export const About: React.FC = () => {
   return (
     <Box w="100%" p="25px" id="about-me">
       <Category title={"About"} />
-      <Grid templateColumns={{ base: "1fr", md: "1fr 1fr" }}>
-        <GridItem>
-          <Flex>
-            <Image
-              src="/images/jakob.jpeg"
-              alt="Jakob Clausen"
-              borderRadius="3px"
-              w={{ base: "200px", md: "350px" }}
-            />
-            <Flex w="100%" pl="20px">
-              <Box w="1px" h="100%" bg="offColor" mr="20px" />
-              <Flex flexDirection="column" justifyContent="space-between">
-                {skills.map((tech) => (
-                  <Text key={tech} fontFamily="body" color="text.main">
-                    {tech}
-                  </Text>
-                ))}
-              </Flex>
-            </Flex>
+      <Flex
+        float={{ base: "none", md: "left" }}
+        p={{ base: "0px 20px 20px 0px", md: "0px 20px 20px 0px" }}
+      >
+        <Image
+          src="/images/jakob.jpeg"
+          alt="Jakob Clausen"
+          borderRadius="3px"
+          w={{ base: "200px", sm: "350px" }}
+        />
+        <Flex w="100%" pl="20px">
+          <Box w="1px" h="100%" bg="offColor" mr="20px" />
+          <Flex flexDirection="column" justifyContent="space-between">
+            {skills.map((tech) => (
+              <Text key={tech} fontFamily="body" color="text.main">
+                {tech}
+              </Text>
+            ))}
           </Flex>
-          <Box pt="20px">
-            <Text fontFamily="body" color="text.main">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-              dicta natus amet, molestiae tenetur impedit aut incidunt quas sunt
-              provident sed minima quis porro modi culpa sequi omnis voluptas
-              aspernatur.
-            </Text>
+        </Flex>
+      </Flex>
+      <Box pt={{ base: "20px", md: "0px" }}>
+        <Text fontFamily="body" color="text.main">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+          dicta natus amet, molestiae tenetur impedit aut incidunt quas sunt
+          provident sed minima quis porro modi culpa sequi omnis voluptas
+          aspernatur. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Dolorem dicta natus amet, molestiae tenetur impedit aut incidunt quas
+          sunt provident sed minima quis porro modi culpa sequi omnis voluptas
+          aspernatur. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Dolorem dicta natus amet, molestiae tenetur impedit aut incidunt quas
+          sunt provident sed minima quis porro modi culpa sequi omnis voluptas
+          aspernatur. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Dolorem dicta natus amet, molestiae tenetur impedit aut incidunt quas
+          sunt provident sed minima quis porro modi culpa sequi omnis voluptas
+          aspernatur. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Dolorem dicta natus amet, molestiae tenetur impedit aut incidunt quas
+          sunt provident sed minima quis porro modi culpa sequi omnis voluptas
+          aspernatur. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Dolorem dicta natus amet, molestiae tenetur impedit aut incidunt quas
+          sunt provident sed minima quis porro modi culpa sequi omnis voluptas
+          aspernatur. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Dolorem dicta natus amet, molestiae tenetur impedit aut incidunt quas
+          sunt provident sed minima quis porro modi culpa sequi omnis voluptas
+          aspernatur. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Dolorem dicta natus amet, molestiae tenetur impedit aut incidunt quas
+          sunt provident sed minima quis porro modi culpa sequi omnis voluptas
+          aspernatur. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Dolorem dicta natus amet, molestiae tenetur impedit aut incidunt quas
+          sunt provident sed minima quis porro modi culpa sequi omnis voluptas
+          aspernatur. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Dolorem dicta natus amet, molestiae tenetur impedit aut incidunt quas
+          sunt provident sed minima quis porro modi culpa sequi omnis voluptas
+          aspernatur. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Dolorem dicta natus amet, molestiae tenetur impedit aut incidunt quas
+          sunt provident sed minima quis porro modi culpa sequi omnis voluptas
+          aspernatur. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Dolorem dicta natus amet, molestiae tenetur impedit aut incidunt quas
+          sunt provident sed minima quis porro modi culpa sequi omnis voluptas
+          aspernatur.
+        </Text>
+      </Box>
+      <Grid
+        templateColumns={{ base: "1fr", md: "1fr 1fr" }}
+        m="40px 0px 20px 0px"
+      >
+        <GridItem>
+          <FooterInfo title={"Contact"} links={contact} />
+        </GridItem>
+        <GridItem>
+          <Box mt={{ base: "20px", md: "0px" }}>
+            <FooterInfo title={"Social"} links={social} />
           </Box>
         </GridItem>
-        <GridItem>
-          <Grid
-            templateColumns={{ base: "1fr", md: "1fr 1fr" }}
-            m={{ base: "20px 0px", md: "0px" }}
-          >
-            <GridItem>
-              <FooterInfo title={"Contact"} links={contact} />
-            </GridItem>
-            <GridItem>
-              <Box mt={{ base: "20px", md: "0px" }}>
-                <FooterInfo title={"Social"} links={social} />
-              </Box>
-            </GridItem>
-          </Grid>
-        </GridItem>
       </Grid>
+
       <Divider opacity={0.2} />
       <Text
         color="text.main"
