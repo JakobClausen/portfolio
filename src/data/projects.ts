df0136bd0b28cbb40c98cf4f8078b676d543f339
Jakob Clausen <jakob.fridmar@gmail.com>
2021-04-11 16:35:38 +0200

    add type of project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,6 +4,7 @@ export const data: Item[] = [
   {
     id: 1,
     title: "Gym hub",
+    type: "Frontend",
     description:
       "A service that help digitalize the gym / crossfit class experiense.\n Making a digital version of a whiteboard and timer, that makes it easy for instructors & class participants to understand the class structure.",
     link: "https://github.com/JakobClausen/ex-timer-web",
@@ -39,6 +40,7 @@ export const data: Item[] = [
   {
     id: 2,
     title: "Gym hub",
+    type: "Backend",
     description:
       "This is the backend / server for the Gym hub service. \n Written in Node js (typescript) & express, using Graphql as a query language. \n Typeorm as an ORM.",
     link: "https://github.com/JakobClausen/ex-timer-server",
@@ -75,6 +77,7 @@ export const data: Item[] = [
   {
     id: 3,
     title: "Portfolio",
+    type: null,
     description:
       "This website is built with Next js in typescript. \n My goals was to try different types of animations, like the Scroll magic logo animation. Some of the libs I use is Framer motion, Scroll magic, GSAP 3. \n For styling i used Chakra ui components.",
     link: "https://github.com/JakobClausen/portfolio",
